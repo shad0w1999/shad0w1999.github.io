967e27820dc7b0703ca6969654b2fb354a19f102
shad0w <shardik99@gmail.com>
2017-01-16 22:03:41 +0530

    Initial Commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-		sectionsColor: ['#BD1A3B', '#1bbc9b', '#ccddff', '#7BAABE', '#4BBFC3'],
+		sectionsColor: ['#BD1A3B', '#1bbc9b', '#3F51B5', '#00BCD4', '#4BBFC3'],
 		css3: true,
 		navigation: true,
 		navigationPosition: 'right',
